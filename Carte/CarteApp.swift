10f1ef7238087eff45f805be6dcf36f8fee29b1f
Guillaume Genest <guillaume-genest2@hotmail.fr>
2023-05-12 16:42:45 +0200

    Initial Commit

---
+++ Carte/CarteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarteApp.swift
+//  Carte
+//
+//  Created by Guillaume Genest on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
